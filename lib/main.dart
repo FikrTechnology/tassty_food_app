f7c7760ffe900ed34b51713dd86743a2ca954d84
rdl-rcd <rdl-rcd@bni.co.id>
2025-07-16 16:45:07 +0700

    Add Font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tassty_food_app/utilities/themes/themes.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            children: [
+              Text(
+                'Selamat Datang!',
+                style: AppTypography.textTheme.headlineLarge,
+              ),
+              Text(
+                'Ini adalah teks body.',
+                style: AppTypography.textTheme.bodyLarge,
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/utilities/themes/text_style.dart
@@ -0,0 +1,53 @@
+part of 'themes.dart';
+class AppTypography {
+  static const String apfelGrotezk = 'ApfelGrotezk';
+  static const String plusJakartaSans = 'PlusJakartaSans';
+
+  static final TextTheme textTheme = TextTheme(
+    // Title Font - apfelGrotezk
+    headlineLarge: TextStyle(
+      fontFamily: apfelGrotezk,
+      fontWeight: FontWeight.w700,
+      fontSize: 32,
+    ),
+    headlineMedium: TextStyle(
+      fontFamily: apfelGrotezk,
+      fontWeight: FontWeight.w500,
+      fontSize: 28,
+    ),
+    headlineSmall: TextStyle(
+      fontFamily: apfelGrotezk,
+      fontWeight: FontWeight.w400,
+      fontSize: 24,
+    ),
+
+    // Body Font - PlusJakartaSans
+    bodyLarge: TextStyle(
+      fontFamily: plusJakartaSans,
+      fontWeight: FontWeight.w400,
+      fontSize: 16,
+    ),
+    bodyMedium: TextStyle(
+      fontFamily: plusJakartaSans,
+      fontWeight: FontWeight.w300,
+      fontSize: 14,
+    ),
+    bodySmall: TextStyle(
+      fontFamily: plusJakartaSans,
+      fontWeight: FontWeight.w300,
+      fontSize: 12,
+    ),
+
+    // Button text
+    labelLarge: TextStyle(
+      fontFamily: plusJakartaSans,
+      fontWeight: FontWeight.w600,
+      fontSize: 14,
+    ),
+    labelMedium: TextStyle(
+      fontFamily: plusJakartaSans,
+      fontWeight: FontWeight.w500,
+      fontSize: 12,
+    ),
+  );
+}
