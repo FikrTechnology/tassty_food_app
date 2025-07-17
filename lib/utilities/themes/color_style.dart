d756dcf230e0c6e955502f7727f75de024089028
rdl-rcd <rdl-rcd@bni.co.id>
2025-07-17 09:42:44 +0700

    Add Color Scheme

---
+++ lib/utilities/themes/color_style.dart
@@ -0,0 +1,61 @@
+part of 'themes.dart';
+
+class AppColors {
+  // PRIMARY (Orange)
+  static const Color orange900 = Color(0xFF643c0d);
+  static const Color orange800 = Color(0xFF834e11);
+  static const Color orange700 = Color(0xFFa96516);
+  static const Color orange600 = Color(0xFFd9811c);
+  static const Color orange500 = Color(0xFFee8e1f);
+  static const Color orange400 = Color(0xFFf1a54c);
+  static const Color orange300 = Color(0xFFf4b369);
+  static const Color orange200 = Color(0xFFf7cb98);
+  static const Color orange100 = Color(0xFFfadcba);
+  static const Color orange50 = Color(0xFFfdf4e9);
+
+  // SUCCESS (Green)
+  static const Color green900 = Color(0xFF2a4d40);
+  static const Color green800 = Color(0xFF376554);
+  static const Color green700 = Color(0xFF47826d);
+  static const Color green600 = Color(0xFF5ba78b);
+  static const Color green500 = Color(0xFF64b799);
+  static const Color green400 = Color(0xFF83c5ad);
+  static const Color green300 = Color(0xFF97cfbb);
+  static const Color green200 = Color(0xFFb8ded0);
+  static const Color green100 = Color(0xFFcfe9df);
+  static const Color green50 = Color(0xFFf0f8f5);
+
+  // DANGER (Pink)
+  static const Color pink900 = Color(0xFF651a3e);
+  static const Color pink800 = Color(0xFF842351);
+  static const Color pink700 = Color(0xFFaa2d69);
+  static const Color pink600 = Color(0xFFda3987);
+  static const Color pink500 = Color(0xFFf03f94);
+  static const Color pink400 = Color(0xFFf365a9);
+  static const Color pink300 = Color(0xFFf57eb7);
+  static const Color pink200 = Color(0xFFf8a7ce);
+  static const Color pink100 = Color(0xFFfac3de);
+  static const Color pink50 = Color(0xFFfeecf4);
+
+  // INFO (Blue)
+  static const Color blue900 = Color(0xFF302268);
+  static const Color blue800 = Color(0xFF3f2d88);
+  static const Color blue700 = Color(0xFF513ab0);
+  static const Color blue600 = Color(0xFF684ae2);
+  static const Color blue500 = Color(0xFF7251f8);
+  static const Color blue400 = Color(0xFF8e74f9);
+  static const Color blue300 = Color(0xFFa18afa);
+  static const Color blue200 = Color(0xFFbeaffc);
+  static const Color blue100 = Color(0xFFd3c9fd);
+  static const Color blue50 = Color(0xFFf1eefe);
+
+  // NEUTRAL (Greyscale)
+  static const Color neutral100 = Color(0xFF1f1f1f);
+  static const Color neutral80 = Color(0xFF444444);
+  static const Color neutral70 = Color(0xFF656565);
+  static const Color neutral60 = Color(0xFF8f8f8f);
+  static const Color neutral40 = Color(0xFF8f8f8f);
+  static const Color neutral30 = Color(0xFFe8e8e8);
+  static const Color neutral20 = Color(0xFFf4f4f7);
+  static const Color neutral10 = Color(0xFFffffff);
+}
--- lib/utilities/themes/themes.dart
+++ lib/utilities/themes/themes.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 part 'text_style.dart';
+part 'color_style.dart';
